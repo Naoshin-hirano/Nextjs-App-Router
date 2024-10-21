2aaf4f08ecf62a4c98232b50e5b3d8176c77f454
Hirano <hnaoya1120@gmail.com>
2024-10-22 00:10:34 +0900

    なぜ2回レンダリングされるかの調査

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /**
+     * 意図しない副作用を検出するために2度レンダリングを行う。デフォルトはtrue
+     * https://www.ey-office.com/blog_archive/2021/06/30/i-found-out-why-the-component-is-drawn-twice-in-the-react/
+     */
+    reactStrictMode: true,
+};
 
 export default nextConfig;
